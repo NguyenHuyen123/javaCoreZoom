eca699e3da800f648fa8ab2dcf1b2b54f296290c
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-08 15:34:26 +0700

    Lab2-3

--- src/main/java/lab2_2/Ex6.java
+++ src/main/java/lab2_2/Ex6.java
@@ -1,6 +1,53 @@
 package lab2_2;
 
+import java.util.Scanner;
+
 public class Ex6 {
-    public static void Sort (){
+    public static void sort (){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhập số nguyên a");
+        int a= sc.nextInt();
+        System.out.println("Nhập số nguyên b");
+        int b= sc.nextInt();
+        System.out.println("Nhập số nguyên c");
+        int c= sc.nextInt();
+        System.out.println("Nhập số nguyên d");
+        int d= sc.nextInt();
+        if(a<b && a<c&& a<d){
+            System.out.print("Tăng dần: "+ a);
+            if (b<c && b<d){
+                System.out.print(b);
+            }else  if (c<d){
+                System.out.print(c);
+            }else System.out.println(d);
+            //
+            if (c<d && c<b){
+                System.out.println(c);
+            }else if (b<d){
+                System.out.println(b);
+
+            }else System.out.println(d);
+            if (d<c && b<c){
+                System.out.println(d);
+            }else if (b<c){
+                System.out.println(b);
+
+            }
+
+        }else if (b<a && b<c && b<d){
+            System.out.print(b);
+
+        }else if (c<a && c<b && c<d){
+            System.out.print(c);
+
+        }else if ( d<a && d<b && d<c){
+            System.out.print(d);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        sort();
     }
 }
--- src/main/java/lab2_2/Ex7.java
+++ src/main/java/lab2_2/Ex7.java
@@ -7,14 +7,14 @@ public class Ex7 {
         Scanner sc= new Scanner(System.in);
         System.out.print("Nhập Kilomet Taxi đã chở bạn: ");
        float km= sc.nextInt();
-        if (km<=1){
+        if (km>0 &&km<=1){
             System.out.println("Số tiền phải trả là "+ km*1500+ " VNĐ");
         }else if (2<=km && km<=5){
-            System.out.println("Số tiền phải trả là "+ km*13500+ " VNĐ");
+            System.out.println( "Số tiền phải trả là "+((1*1500)+(km-2)*13500)+ " VNĐ");
         }else if(km>=6&& km<=120){
-            System.out.println("Số tiền phải trả là "+ km*11000+ " VNĐ");
+            System.out.println("Số tiền phải trả là "+((1*1500)+(5*13500)+ ((km-6)*11000))+ " VNĐ");
         }else {
-            System.out.println("Số tiền phải trả là "+ km*11000*0.1+ " VNĐ");
+            System.out.println("Số tiền phải trả là "+((1*1500)+(5*13500)+(120*11000) +(km-121)*11000*0.1)+ " VNĐ");
         }
     }
 
