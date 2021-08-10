b1d7e7cc77dac2c7da636fa752ea12c92dbfd263
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-10 15:51:14 +0700

    Lab3

--- src/main/java/Lab3_3/Ex5.java
+++ src/main/java/Lab3_3/Ex5.java
@@ -3,11 +3,17 @@ package Lab3_3;
 public class Ex5 {
 
     public String reverseNumber(int n) {
-        String s="";
-        s= String.valueOf(n);
-        StringBuilder str= new StringBuilder(s);
-        return str.reverse()+" ";
-
+//        String s="";
+//        s= String.valueOf(n);
+//        StringBuilder str= new StringBuilder(s);
+//        return str.reverse()+" ";
+        String s= String.valueOf(n);
+        char[] reverse= s.toCharArray();
+        String kq=" ";
+        for (int i =reverse.length-1; i >=0 ; i--) {
+            kq+=reverse[i]+" ";
+        }
+        return kq;
     }
 
 }
