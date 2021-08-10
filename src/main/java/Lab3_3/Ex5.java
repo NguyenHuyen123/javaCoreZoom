8592379e2e393df28d0f65840fde65bc2422238c
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-10 16:14:17 +0700

    Lab3

--- src/main/java/Lab3_3/Ex5.java
+++ src/main/java/Lab3_3/Ex5.java
@@ -11,7 +11,7 @@ public class Ex5 {
         char[] reverse= s.toCharArray();
         String kq=" ";
         for (int i =reverse.length-1; i >=0 ; i--) {
-            kq+=reverse[i]+" ";
+            kq+=reverse[i]+"";
         }
         return kq;
     }
