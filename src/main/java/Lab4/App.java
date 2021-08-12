0a43612981913d99d0d0ff64117c4c2db3a60843
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-12 16:09:51 +0700

    Lab4

--- src/main/java/Lab4/App.java
+++ src/main/java/Lab4/App.java
@@ -3,7 +3,7 @@ package Lab4;
 public class App {
     public static void main(String[] args) {
         Ex2 e= new Ex2();
-        int []arr= {2,3,5,7,16};
+        int []arr= {2,3,5,7,16,24,79};
         System.out.println("Toàn bảng là số nguyên tố: "+ e.isAllPrime(arr));
         System.out.println("Có phải mảng tăng dần không? "+ e.arrAscending(arr));
         //3a
