5e29cc2b19577f839088c8352db78cbe447c568b
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-25 18:19:29 +0700

    OOP

---
+++ src/main/java/oOp/App.java
@@ -0,0 +1,38 @@
+package oOp;
+
+import java.util.Scanner;
+
+public class App {
+    /* Phân số
+§ Khai báo kiểu dữ liệu phân số (PHANSO)
+§ Nhập/Xuất phân số
+§ Rút gọn phân số
+§ Tính tổng, hiệu, tích, thương hai phân số
+§ Kiểm tra phân số tối giản
+§ Quy đồng hai phân số
+§ Kiểm tra phân số âm hay dương
+§ So sánh hai phân số
+ */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tử số: ");
+        int numera1 = sc.nextInt();
+        System.out.println("Nhập mẫu số: ");
+        int denom1 = sc.nextInt();
+        System.out.println("Nhập tử số: ");
+        int numera2 = sc.nextInt();
+        System.out.println("Nhập mẫu số: ");
+        int denom2 = sc.nextInt();
+        Fraction fc1 = new Fraction(numera1, denom1);
+        Fraction fc2 = new Fraction(numera2, denom2);
+        fc1.sumFrac(fc2);
+        fc1.subFrac(fc2);
+        fc1.multipFrac(fc2);
+        fc1.diviFrac(fc2);
+        System.out.println(fc1.isreduceFrac());
+        System.out.println(fc1.lunisolarFrac());
+        fc1.compareFraction(fc2);
+
+    }
+
+}
---
+++ src/main/java/oOp/Fraction.java
@@ -0,0 +1,89 @@
+package oOp;
+
+public class Fraction {
+    private int numerator;
+    private int denomerator;
+
+    public Fraction(int numerator, int denomerator) {
+        this.numerator = numerator;
+        this.denomerator = denomerator;
+    }
+    public boolean isFrac(){
+        if ( denomerator==0){
+            return  false;
+        }else
+            return true;
+    }
+
+    private int USCLN(int a, int b) {
+        if (b == 0) return a;
+        return USCLN(b, a % b);
+    }
+
+    public void reduceFrac() {
+        int i = USCLN(numerator, denomerator);
+        numerator /= i;
+        denomerator /= i;
+    }
+
+    public void sumFrac(Fraction ps) {
+        int ts = this.numerator * ps.denomerator + ps.numerator * this.denomerator;
+        int ms = this.denomerator * ps.denomerator;
+        Fraction sumFrac = new Fraction(ts, ms);
+        sumFrac.reduceFrac();
+
+        System.out.println("Cộng 2 phân số: " + sumFrac.numerator + "/" + sumFrac.denomerator);
+    }
+
+    public void subFrac(Fraction ps) {
+        int ts = this.numerator * ps.denomerator -  this.denomerator*ps.numerator;
+        int ms = this.denomerator * ps.denomerator;
+        Fraction subfrac = new Fraction(ts, ms);
+        subfrac.reduceFrac();
+        System.out.println("Hiệu 2 phân số: " + subfrac.numerator + "/" + subfrac.denomerator);
+    }
+
+    public void multipFrac(Fraction ps) {
+        int ts = this.numerator * ps.numerator;
+        int ms = this.denomerator * ps.denomerator;
+        Fraction multiFrac = new Fraction(ts, ms);
+        multiFrac.reduceFrac();
+        System.out.println("Nhân 2 phân số: " + multiFrac.numerator + "/" + multiFrac.denomerator);
+    }
+
+    public void diviFrac(Fraction ps) {
+        if (isFrac()==true) {
+            int ts = this.numerator * ps.denomerator;
+            int ms = this.denomerator * ps.numerator;
+
+            Fraction diviFrac = new Fraction(ts, ms);
+            diviFrac.reduceFrac();
+
+            System.out.println("Thương 2 phân số: " + diviFrac.numerator + "/" + diviFrac.denomerator);
+        }
+    }
+
+    public boolean isreduceFrac() {
+        if (USCLN(this.numerator, denomerator) == 1) {
+            return true;
+        }
+        return false;
+    }
+    public boolean lunisolarFrac (){
+        if (this.numerator/this.denomerator >0){
+            return  true;// là phân số dương
+        }else
+            return  false;//là phân số âm
+
+    }
+    public void compareFraction(Fraction ps1){
+        double p1=(double) this.numerator / (double)this.denomerator;
+        double p2= (double) ps1.numerator/(double) ps1.denomerator;
+        if (p1>p2){
+            System.out.println("phân số "+this.numerator +"/"+ this.denomerator+"lớn hơn "+ ps1.numerator +"/"+ ps1.denomerator);
+        }else {
+            System.out.println("phân số "+ps1.numerator +"/"+ ps1.denomerator+" lớn hơn "+ this.numerator +"/"+ this.denomerator);
+        }
+    }
+
+}
