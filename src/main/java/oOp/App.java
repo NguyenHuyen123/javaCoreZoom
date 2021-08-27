e11930a7cbeffaff865115a946df6d26fbca8740
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-08-27 12:44:35 +0700

    OOP

--- src/main/java/oOp/App.java
+++ src/main/java/oOp/App.java
@@ -14,7 +14,9 @@ public class App {
 § So sánh hai phân số
  */
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Nhập tử số: ");
         int numera1 = sc.nextInt();
         System.out.print("Nhập mẫu số: ");
@@ -35,11 +37,15 @@ public class App {
         System.out.println(fc1.lunisolarFrac());
         fc1.compareFraction(fc2);
 
+
+
         /*Tam giác
         //§ Khai báo kiểu dữ lịêu tam giác (TAMGIAC)
         //§ Nhập/Xuất tam giác
         //§ Tính chu vi, diện tích tam giác
+
          */
+
         System.out.println("====Tính chu vi , diện tách tam giác");
         System.out.println("Nhập canh a");
         int a = sc.nextInt();
@@ -47,9 +53,37 @@ public class App {
         int b = sc.nextInt();
         System.out.println("Nhập canh c");
         int c = sc.nextInt();
-        Triangle t= new Triangle(a,b,c);
-        System.out.println("Chu vi tam giác là: "+t.chuVi());
-        System.out.println("Diện tích tam giác:"+ t.dienTich());
+        Triangle t = new Triangle(a, b, c);
+        System.out.println("Chu vi tam giác là: " + t.chuVi());
+        System.out.println("Diện tích tam giác:" + t.dienTich());
+        /*Tam giác
+       Điểm trong mặt phẳng Oxy
+       Khai báo kiểu dữ liệu điểm (DIEM)
+       Nhập/Xuất tọa độ điểm
+       Tính khoảng cách giữa hai điểm
+       Tìm điểm đối xứng qua gốc toạ độ/trục Ox/Oy
+§      Kiểm tra điểm thuộc phần tư nào?
+         */
 
+        System.out.println("Nhập tọa độ x:");
+        int x1 = sc.nextInt();
+        System.out.println("Nhập tọa độ y");
+        int y1 = sc.nextInt();
+        System.out.println("Nhập tọa độ x:");
+        int x2 = sc.nextInt();
+        System.out.println("Nhập tọa độ y");
+        int y2 = sc.nextInt();
+        Oxy oxy1 = new Oxy(x1, y1);
+        Oxy oxy2 = new Oxy(x2, y2);
+        if (oxy1.isOrigin() == true) {
+            System.out.println("Là gốc tọa độ");
+        } else
+            System.out.println("Khoảng cách giữa 2 điểm :" + oxy1.distance(oxy2));
+        //
+        oxy1.symmetryOx();
+        oxy1.symmetryOy();
+        oxy1.symmetryO();
+        oxy1.quarterOxy();
     }
+
 }
---
+++ src/main/java/oOp/Oxy.java
@@ -0,0 +1,75 @@
+package oOp;
+
+public class Oxy {
+    int ox, oy;
+
+    public Oxy(int ox, int oy) {
+        this.ox = ox;
+        this.oy = oy;
+    }
+
+    public int getOx() {
+        return ox;
+    }
+
+    public int getOy() {
+        return oy;
+    }
+
+    @Override
+    public String toString() {
+        return "A(" + this.ox + "," + this.oy + ")";
+    }
+
+    public boolean isOrigin() {
+        if (this.ox == 0 && this.oy == 0) {
+            return true; // là gốc tọa độ
+        } else {
+            return false; // Không phải gốc tọa độ
+        }
+    }
+
+    public double distance(Oxy o) {// tính khoảng cách 2 điểm
+        double x = Math.pow(this.ox - o.ox, 2);
+        double y = Math.pow(this.oy - o.oy, 2);
+        double result = Math.sqrt(x + y);
+        return result;
+    }
+
+    public void symmetryOx() { //tìm điểm đối xứng qua  trục hoành
+        int x = this.ox;
+        int y = -this.oy;
+        System.out.println(toString() + " =>" + "A(" + x + "," + y + ")");
+
+    }
+
+    public void symmetryOy() { //tìm điểm đối xứng qua  trục tung
+        int x = -this.ox;
+        int y = this.oy;
+        System.out.println(toString() + " =>" + "A(" + x + "," + y + ")");
+
+    }
+
+    public void symmetryO() { //tìm điểm đối xứng qua gỗ tọa độ
+        int x = -this.ox;
+        int y = -this.oy;
+        System.out.println(toString() + " =>" + "A(" + x + "," + y + ")");
+
+    }
+
+    public void quarterOxy() {
+        if (isOrigin() == true) {
+            System.out.println(toString() + "Là gốc tọa độ");
+        } else if (this.ox > 0 && this.oy > 0) {
+            System.out.println(toString() + " Là phần tư I");
+        } else if (this.ox < 0 && this.oy < 0) {
+            System.out.println(toString() + " Là phần tư III");
+        } else if (this.ox < 0 && this.oy > 0) {
+            System.out.println(toString() + " Là phần tư II");
+        } else
+            System.out.println(toString() + " Là phần tư IV");
+
+    }
+
+
+}
